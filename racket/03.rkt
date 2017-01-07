e7ea508f528e130d74bac934b52bfdb4cce05f2f
Luiz de Milon <luiz.de.milon@gmail.com>
2017-01-07 19:41:19 -0300

    Use the recursion macro.

--- racket/03.rkt
+++ racket/03.rkt
@@ -1,18 +1,18 @@
 #lang racket
+
+(require "recurse.rkt")
+
 ; Problem #3
 ; The prime factors of 13195 are 5, 7, 13 and 29.
 ; What is the largest prime factor of the number 600851475143 ?
 
-; factorize n via brute force
-(define factorize
-  (lambda (n)
-    (letrec
-      ((aux (lambda (x div out)
-              (if (= x 1)
-                out
-                (if (= 0 (remainder x div))
-                  (aux (/ x div) div (cons div out))
-                  (aux x (+ div 1) out))))))
-       (aux n 2 '()))))
+(define (factorize n)
+  (recurse (aux x div out) (n 2 '())
+    (if (= x 1)
+      out
+      (if (= 0 (remainder x div))
+        (aux (/ x div) div (cons div out))
+        (aux x (+ div 1) out)))))
 
-(display (car (factorize 600851475143)))
+; Solution
+(car (factorize 600851475143))
