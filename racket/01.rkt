83fb44dab70f6099130c89cd6e847592ad1910e9
Luiz de Milon <luiz.de.milon@gmail.com>
2017-11-19 12:19:17 -0300

    01: use fold directly

--- racket/01.rkt
+++ racket/01.rkt
@@ -5,13 +5,10 @@
 
 ; Find the sum of all the multiples of 3 or 5 below 1000.
 
-(define (sum l)
-  (foldr + 0 l))
-
 (define (fizzbuzz? n)
   (or
     (= 0 (remainder n 3))
     (= 0 (remainder n 5))))
 
 ; Solution
-(sum (filter fizzbuzz? (range 1000)))
+(foldr + 0 (filter fizzbuzz? (range 1000)))
