092399dea4f5ff18c32cc313c43de9ed554b7433
Luiz de Milon <luiz.de.milon@gmail.com>
2017-11-19 12:20:38 -0300

    06: use fold directly

--- racket/06.rkt
+++ racket/06.rkt
@@ -4,10 +4,7 @@
 ; Find the difference between the sum of the squares of
 ; the first one hundred natural numbers and the square of the sum.
 
-(define (sum l)
-  (foldr + 0 l))
-
 ; Solution
 (let [(l (range 100))]
-  (- (sqr (sum l))
-     (sum (map sqr l))))
+  (- (sqr (foldr + 0 l))
+     (foldr + 0 (map sqr l))))
