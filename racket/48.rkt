509d9165afa5e119cb8e0ac934a1c69c42217a82
Luiz de Milon <luiz.de.milon@gmail.com>
2017-11-19 12:54:09 -0300

    48: make it clearer and use aux.rkt

--- racket/48.rkt
+++ racket/48.rkt
@@ -5,15 +5,12 @@
 
 ; Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 
+(require "aux.rkt")
+
 (define (self-power n)
   (expt n n))
 
-(define (integer->list n)
-  (let aux ((in n) (out '()))
-    (if (zero? in)
-      out
-      (aux (quotient in 10) (cons (remainder in 10) out)))))
-
 ; Solution
-(let [(series-sum (integer->list (foldl + 0 (map self-power (range 1 1001)))))]
-  (drop series-sum (- (length series-sum) 10)))
+(let* [(sum (foldl + 0 (map self-power (range 1 1001))))
+       (digits (integer->list sum))]
+  (list->integer (drop digits (- (length digits) 10))))
